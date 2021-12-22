0d862512c6bff5e2a56bad38035a9fc4f749004b
Aiden Robinson <96023835+Common-Crow23@users.noreply.github.com>
2021-12-22 09:54:44 -0500

    Create generator.kt

---
+++ generator.kt
@@ -0,0 +1,24 @@
+import kotlin.random.Random
+
+fun getRandPassword(n: Int): String
+{
+    val characterSet = "123456789abcdefghijklmnopqrstuvwxyz"
+    
+    val random = Random(System.nanoTime())
+    val password = StringBuilder()
+    
+    for (i in 0 until n)
+    {
+        val rIndex = random .nextInt(characterSet.length)
+        password.append(characterSet[rIndex])
+    }
+    
+    return password.toString()
+}
+
+fun main() {
+    val n = 8
+    println(getRandPassword(n))
+}
+                     
+                    
