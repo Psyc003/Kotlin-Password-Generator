50cee4b2f6fac00bdd7b20c157dc2ddc3294dc70
codefactor-io <support@codefactor.io>
2022-01-01 23:03:19 +0000

    [CodeFactor] Apply fixes

--- generator.kt
+++ generator.kt
@@ -3,16 +3,16 @@ import kotlin.random.Random
 fun getRandPassword(n: Int): String
 {
     val characterSet = "123456789abcdefghijklmnopqrstuvwxyz"
-    
+
     val random = Random(System.nanoTime())
     val password = StringBuilder()
-    
+
     for (i in 0 until n)
     {
         val rIndex = random .nextInt(characterSet.length)
         password.append(characterSet[rIndex])
     }
-    
+
     return password.toString()
 }
 
@@ -20,5 +20,5 @@ fun main() {
     val n = 8
     println(getRandPassword(n))
 }
-                     
-                    
+
+
